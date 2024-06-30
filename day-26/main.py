ceafa900b7ed743fd3e311fb359dfccad64f3e01
Harsha Yadav <harshay1229@gmail.com>
2024-06-30 20:29:10 +0530

    Added generate_phonetic function with exception handling

--- day-26/main.py
+++ day-26/main.py
@@ -7,6 +7,17 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(data_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-name = input("Enter your name: ").upper()
-nato_dict = {letter: data_dict[letter] for letter in name }
-print(nato_dict)
+
+
+def generate_phonetic():
+    name = input("Enter your name: ").upper()
+    try:
+        nato_dict = {letter: data_dict[letter] for letter in name if letter != " "}
+    except KeyError:
+        print('Sorry, only write alphabets to get the result.')
+        generate_phonetic()
+    else:
+        print(nato_dict)
+
+
+generate_phonetic()
