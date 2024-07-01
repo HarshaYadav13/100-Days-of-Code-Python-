40a6f4d55d110a0396798ec4c177d6e3056a21fc
Harsha Yadav <harshay1229@gmail.com>
2024-07-01 10:57:01 +0530

    Added search button

--- day-30/main.py
+++ day-30/main.py
@@ -71,6 +71,22 @@ def save():
     website_input.focus()
 
 
+def find_password():
+    website = website_input.get()
+    try:
+        with open('password.json', 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found.")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f"email: {email}\npassword: {password}")
+        else:
+            messagebox.showinfo(title="Not Found", message="No details for this website exists.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -82,29 +98,32 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website: ", padx=5, pady=5)
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky='e')  # for right-allignment
 
-website_input = Entry(width=35)
+website_input = Entry(width=33)
 website_input.focus()  # to set the cursor in this text field
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1, sticky='w')  # for left-allignment
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky='w')
 
 email_label = Label(text="Email/Username:", padx=5, pady=5)
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky='e')
 
-email_input = Entry(width=35)
+email_input = Entry(width=52)
 email_input.insert(END, string="@gmail.com")
-email_input.grid(row=2, column=1, columnspan=2)
+email_input.grid(row=2, column=1, columnspan=2, sticky='w')
 
 password_label = Label(text="Password:", padx=5, pady=5)
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky='e')
 
-password_input = Entry(width=22)
-password_input.grid(row=3, column=1)
+password_input = Entry(width=33)
+password_input.grid(row=3, column=1, sticky='w')
 
 password_button = Button(text="Generate Password", command=generate_password)
-password_button.grid(row=3, column=2)
+password_button.grid(row=3, column=2, sticky='w')
 
-add_button = Button(text="Add", width=36, padx=3, pady=3, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button = Button(text="Add", width=43, padx=3, pady=3, command=save)
+add_button.grid(row=4, column=1, columnspan=2, sticky='w')
 
 window.mainloop()
